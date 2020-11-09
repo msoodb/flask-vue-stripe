43c17bb0445f910f688d3ad8526a137e2dbb178b
msoodb <masoud.bolhassani@gmail.com>
2020-11-09 14:35:26 +0200

    remove script loader

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import BootstrapVue from 'bootstrap-vue';
-import LoadScript from 'vue-plugin-load-script';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
 
-Vue.use(LoadScript);
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
